47728f6742becfccacf86009e90eebf675ce1420
Wu Wenwu <wen.v.wu@gmail.com>
2016-06-17 19:31:41 +0800

    .

---
+++ test/test.js
@@ -0,0 +1,18 @@
+
+var chai   = require('chai'),
+    expect = chai.expect;
+
+var tests = {
+};
+
+
+describe('Package', function () {
+
+    it('Basic info of Package', function () {
+        // expect(1).to.equal(1);
+    });
+
+    it('Test 1', function () {
+        console.log("No test case.");
+    });
+});
