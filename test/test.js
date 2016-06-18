3ec4a1666eb29a14ec8f11d3c090217d9b63aea1
Wu Wenwu <wen.v.wu@gmail.com>
2016-06-18 11:00:23 +0800

    .

--- test/test.js
+++ test/test.js
@@ -7,12 +7,14 @@ var tests = {
 
 
 describe('Package', function () {
+    console.log("No test case.");
 
+    /*
     it('Basic info of Package', function () {
         // expect(1).to.equal(1);
     });
 
     it('Test 1', function () {
-        console.log("No test case.");
     });
+    */
 });
